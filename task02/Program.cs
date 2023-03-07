5575695bc4966138d7536550e52e653f3dc0cdcb
Anastasia_Anast <anastasia.burskaya@yandex.ru>
2023-03-07 12:08:21 +0300

    Добавили решение к задаче два для самостоятельного решения

--- task02/Program.cs
+++ task02/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+// Console.Write("Введите целое число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int square = number * number;
+
+// Задаем целочисленную переменную numberDay
+// если пользователь вводит 1 - консоль выдает "понедельник"
+// если пользователь вводит 2 - консоль выдает "вторник"
+// если пользователь вводит 3 - консоль выдает "среда"
+// если пользователь вводит 4 - консоль выдает "четверг"
+// если пользователь вводит 5 - консоль выдает "пятница"
+// если пользователь вводит 6 - консоль выдает "суббота"
+// если пользователь вводит 7 - консоль выдает "воскресенье"
+// иначе - консоль выдает "такого дня недели не существует"
+
+Console.WriteLine("Введите номер дня: ");
+string numberDay = Console.ReadLine();
+    if (numberDay == "1")
+{
+    Console.WriteLine("понедельник");
+}
+    if (numberDay == "2")
+{
+    Console.WriteLine("вторник");
+}
+    if (numberDay == "3")
+{
+    Console.WriteLine("среда");
+}
+    if (numberDay == "4")
+{
+    Console.WriteLine("четверг");
+}
+    if (numberDay == "5")
+{
+    Console.WriteLine("пятница");
+}
+    if (numberDay == "6")
+{
+    Console.WriteLine("суббота");
+}
+    if (numberDay == "7")
+{
+    Console.WriteLine("воскресенье");
+}
+else 
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
