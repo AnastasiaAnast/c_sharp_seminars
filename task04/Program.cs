952969e5b3ed2dfe871fafefed8bed2d66f9bdd2
Anastasia_Anast <anastasia.burskaya@yandex.ru>
2023-03-07 12:37:32 +0300

    Добавили решение 4-й задачи

--- task04/Program.cs
+++ task04/Program.cs
@@ -5,3 +5,15 @@
 // 782 -> 2
 // 918 -> 8
 
+Console.Write ("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num >= 100 && < 1000);
+{
+int numLast = num $ 10;
+Console.WriteLine(numLast);
+}
+else
+{
+    Console.WriteLine ("Введите трехзначное число")
+}
