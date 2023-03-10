f181c2f6fd4c45118eb0fdf3f3a5f76c07aec0d6
Anastasia_Anast <anastasia.burskaya@yandex.ru>
2023-03-10 14:41:05 +0300

    Внесли небольшие корректировки в формулировку ответа по ДЗ по семинару 1, задача6

--- task6HW/Program.cs
+++ task6HW/Program.cs
@@ -6,9 +6,9 @@ Console.WriteLine ("Введите число: ");
 double num = Convert.ToDouble(Console.ReadLine());
 if (num % 2 == 0)
 {
-Console.WriteLine($"указанное число четное");
+Console.WriteLine($"указанное число является четным");
 }
 else
 {
-Console.WriteLine($"указанное число нечетное");
+Console.WriteLine($"указанное число не является четным");
 }
