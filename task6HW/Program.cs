02992374e12893ba339498b6ed5adceea62dcaaa
Anastasia_Anast <anastasia.burskaya@yandex.ru>
2023-03-10 14:23:54 +0300

    Добавили рещение ДЗ после семинара1, задача6

---
+++ task6HW/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая 
+// на вход принимает число и 
+// выдает, явл ли оно четным (делится на 2 баз остатка)
+
+Console.WriteLine ("Введите число: ");
+double num = Convert.ToDouble(Console.ReadLine());
+if (num % 2 == 0)
+{
+Console.WriteLine($"указанное число четное");
+}
+else
+{
+Console.WriteLine($"указанное число нечетное");
+}
