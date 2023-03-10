cf8a41198fb381fd61640e96364650f0d012d875
Anastasia_Anast <anastasia.burskaya@yandex.ru>
2023-03-10 16:11:50 +0300

    Добавили предварительное решение (с диапазоном 100-999) для ДЗ семинара2, задача 10

---
+++ task10HW/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая 
+// принимает на вход трехзначное число и 
+// на выходе показывает вторую цифру из этого числа
+
+Console.WriteLine ("Введите трехзначное число, чтобы увидеть вторую цифру из данного числа. Обратите внимание, что если число будет не трехзначным, программа работать не будет.");
+
+int SecondDigitOfAThreeDigitNumber(int num)
+{
+    int firstStep = num / 10;
+    int lastStep = firstStep % 10;
+    return lastStep;
+}
+
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 100 && num <= 999) 
+{
+    int solution = SecondDigitOfAThreeDigitNumber(num);
+    Console.WriteLine($"Вторая цифра введенного числа {num} - {solution}");
+}
+else
+{
+    Console.WriteLine("Введенное число не является трехзначным");
+}
+
