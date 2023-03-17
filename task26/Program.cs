e7112c031efe5a987e8a48af9b328c53e75473a9
Anastasia_Anast <anastasia.burskaya@yandex.ru>
2023-03-17 11:07:05 +0300

    Добавили решение задачи 26

--- task26/Program.cs
+++ task26/Program.cs
@@ -7,8 +7,18 @@
 
 Console.WriteLine("Введите целое число");
 int number = Convert.ToInt32(Console.ReadLine());
+int countDigit = CountDigit(number);
 
-int CountOfNumber()
+Console.WriteLine($"Кол-во цифр в числе {number} - {countDigit}");
+
+int CountDigit(int num)
 {
+    int count = 0;
 
+    while (num != 0)
+    {
+        count++;
+        num /= 10;
+    }
+    return count;
 }
