d2f9719f8cfc1b5788b5528a91dcdbd858700d85
Anastasia_Anast <anastasia.burskaya@yandex.ru>
2023-04-04 09:21:46 +0300

    Добавили решение домашних задач для семинара3

--- task19HW/Program.cs
+++ task19HW/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+bool Palindrome(int number)
+{
+    if(number < 10) 
+    return false;
+
+    int initialNumber = number;
+    int reversedNumber = 0;
+
+    while(number > 0)
+    {
+        reversedNumber = reversedNumber * 10 + number % 10;
+        number /= 10;
+    }
+    return initialNumber == reversedNumber;
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num < 0) 
+num = num * -1;
+
+bool palindrome = Palindrome(num);
+Console.WriteLine(palindrome ? "Введенное число является палиндромом" : "Введенное число не является палиндромом");
--- task23HW/Program.cs
+++ task23HW/Program.cs
@@ -1,3 +1,21 @@
 ﻿// Напишите программу, которая 
 // принимает на вход число (N) и 
 // выдает таблицу кубов чисел от 1 до N
+
+void CubeTable(int number)
+{
+    int count = 1;
+while(count <= number)
+    {
+    int cube = count * count * count;
+    Console.WriteLine($"{count,4} -> {cube,8}");
+    count++;
+    }
+}
+
+
+Console.WriteLine("Введите натуральное число, чтобы увидеть таблицу кубов чисел от 1 до указанного числа: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+CubeTable(num);
