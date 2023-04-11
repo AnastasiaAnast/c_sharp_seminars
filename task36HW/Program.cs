d4f3fed4d4b1f780be8e3fc3fcb7b5b25a055e32
Anastasia_Anast <anastasia.burskaya@yandex.ru>
2023-04-11 09:36:17 +0300

    Добавили решение домашних задач 34, 36, 38

---
+++ task36HW/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте одномерный массив, 
+// заполненный случайными числами. 
+// Найдите сумму элементов, 
+// стоящих на нечётных позициях.
+
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    Console.Write("Сейчас одномерный массив, заполненный случайными числами такой - [");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int SumElemOddPosition(int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if(i % 2 != 0) sum = sum + arr[i];
+    }
+    return sum;
+}
+
+int[] array = CreateArray(19, 0, 28);
+PrintArray(array);
+Console.WriteLine();
+int sumElemOddPosition = SumElemOddPosition(array);
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях = {sumElemOddPosition}");
---
+++ task38HW/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и 
+// минимальным элементов массива.
+
+double[] CreateArrayDoubleNum(int size, int min, int max, int round = 1)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        double num = rnd.NextDouble() * (max - min) + min;
+        arr[i] = Math.Round(num, round);
+    }
+    return arr;
+}
+
+void PrintArray (double[] arr)
+{
+    Console.Write("Сейчас массив вещественных чисел такой - [");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+double MaxElement(double[] arr)
+{
+    double max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+
+double MinElement(double[] arr)
+{
+    double min = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] < min) min = arr[i];
+    }
+    return min;
+}
+
+double DifferenceMaxMin(double max, double min)
+{
+    double dif = max - min;
+    dif = Math.Round(dif, 1);
+    return dif;
+}
+
+double[] array = CreateArrayDoubleNum(10, 0, 100, 1);
+double maxElement = MaxElement(array);
+double minElement = MinElement(array);
+double diffMaxMin = DifferenceMaxMin(maxElement, minElement);
+
+PrintArray(array);
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива = {diffMaxMin}");
